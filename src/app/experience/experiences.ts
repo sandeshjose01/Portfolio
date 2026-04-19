c7c8090d7d0d37b3ba05a32854fdd5bf3b5bba7c
sandeshjose01 <116247192+sandeshjose01@users.noreply.github.com>
2026-04-19 22:28:10 +0545

    Update experiences.ts

--- src/app/experience/experiences.ts
+++ src/app/experience/experiences.ts
@@ -21,7 +21,7 @@ export interface Recommendation {
 // --- 1. PERSONAL INFORMATION (Updates your Name & PDF Header) ---
 export const personalInfo = {
   name: { first: "SANDESH", last: "JOSHI" },
-  profileImage: "@public/profile.png",
+  profileImage: "/profile.png",
   role: "CREATIVE GRAPHIC DESIGNER & VIDEO EDITOR",
   contact: {
     location: "Changunarayan-2, Bhaktapur, Nepal",
