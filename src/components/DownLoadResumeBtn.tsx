ee921e92f770c5c1d7e4991b5cf3be8667b058f1
sandeshjose01 <116247192+sandeshjose01@users.noreply.github.com>
2026-04-19 20:35:08 +0545

    Update DownLoadResumeBtn.tsx

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import HackerBtn from './animation/HackerBtn'
-import { downloadATSResume } from "@/lib/generateResume";
 
 function DownLoadResumeBtn() {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
-      {/* Passing the event 'e' to the function */}
-      <div 
-        onClick={(e) => downloadATSResume(e)} 
-        className="cursor-pointer w-fit"
-      >
+      {/* 
+         REMOVED the onClick from here to prevent double downloading. 
+         The HackerBtn inside handles the click now.
+      */}
+      <div className="w-fit">
         <HackerBtn label='Download Resume' />
       </div>
     </div>
