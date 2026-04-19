86f5bce8e7b244980efbf66f4e97de7488ad7356
sandeshjose01 <116247192+sandeshjose01@users.noreply.github.com>
2026-04-19 18:14:44 +0545

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,6 @@ import { Dock, DockIcon, DockItem, DockLabel } from '@/components/animation/Dock
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import FramerWrapper from "./animation/FramerWrapper";
 import { usePathname } from "next/navigation";
 
 const Navbar = () => {
@@ -24,37 +23,42 @@ const Navbar = () => {
   const data = [
     {
       title: 'Home',
-      icon: <HomeIcon className='h-full w-full ' />,
+      icon: <HomeIcon className='h-full w-full' />,
       href: '/',
     },
     {
       title: 'About',
-      icon: <User className='h-full w-full ' />,
+      icon: <User className='h-full w-full' />,
       href: '/about',
     },
     {
       title: 'Skills',
-      icon: <LightbulbIcon className='h-full w-full ' />,
+      icon: <LightbulbIcon className='h-full w-full' />,
       href: '/skills',
     },
     {
       title: 'Education',
-      icon: <GraduationCap className='h-full w-full ' />,
+      icon: <GraduationCap className='h-full w-full' />,
       href: '/education',
     },
     {
+      title: 'Experience', // ADDED EXPERIENCE PAGE HERE
+      icon: <Briefcase className='h-full w-full' />,
+      href: '/experience',
+    },
+    {
       title: 'Projects',
-      icon: <FolderGit2 className='h-full w-full ' />,
+      icon: <FolderGit2 className='h-full w-full' />,
       href: '/projects',
     },
     {
       title: 'Contact me',
-      icon: <Mail className='h-full w-full ' />,
+      icon: <Mail className='h-full w-full' />,
       href: '/contact',
     },
     {
       title: 'More',
-      icon: <MoreHorizontal className='h-full w-full ' />,
+      icon: <MoreHorizontal className='h-full w-full' />,
       href: '/more',
     },
   ];
@@ -81,7 +85,6 @@ const Navbar = () => {
   return (
     <div className={`fixed bottom-5 right-0 left-0 px-0 sm:px-5 m-auto w-full sm:w-fit bg-transparent z-[+9999999] transition-opacity duration-300 ${scrolling ? "hidden":"block"}`}>
       
-      {/* ADDED '!' TO FORCE THE OVERRIDE */}
       <Dock className='items-end pb-3 rounded-full !bg-white/40 !backdrop-blur-xl border !border-white/60 shadow-md'>
         {data.map((item, idx) => (
           <Link href={item.href} key={idx}>
