1ef662c6b1a76e233a8085095f2906ec9d98c4dc
sandeshjose01 <116247192+sandeshjose01@users.noreply.github.com>
2026-04-30 00:02:31 +0545

    Update HeroTexts.tsx

--- src/components/HeroTexts.tsx
+++ src/components/HeroTexts.tsx
@@ -3,20 +3,23 @@ import React from "react";
 import TextRotator from "./TextRotator";
 
 interface HeroTextsProps {
-  staticRole?: string;
   roles?: string[];
   name?: string;
 }
 
-const HeroTexts = ({ staticRole, roles, name }: HeroTextsProps) => {
+const HeroTexts = ({ roles, name }: HeroTextsProps) => {
   return (
     <>
-      <h3 className="font-poppins text-2xl max-sm:text-xl text-white/70">
-        {staticRole || "My Name is"}
+      {/* // text: Hardcoded to always show defaultly as requested */}
+      <h3 className="font-poppins text-2xl max-sm:text-xl text-white">
+        My Name is
       </h3>
+      
       <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl text-white">
+        {/* // text: Shows Admin name or default */}
         {name || "Sandesh Joshi"} .
       </h1>
+      
       <TextRotator items={roles || []} />
     </>
   );
