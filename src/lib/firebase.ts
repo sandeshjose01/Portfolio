cba0651141de32ce5520243b0cf11fa866c13517
sandeshjose01 <116247192+sandeshjose01@users.noreply.github.com>
2026-04-29 21:12:03 +0545

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCChQzdSuNW8EzBXtRxZyvwh4WOjj67FCs",
+    authDomain: "my-portfolio-96764.firebaseapp.com",
+    projectId: "my-portfolio-96764",
+    storageBucket: "my-portfolio-96764.firebasestorage.app",
+    messagingSenderId: "1018154031940",
+    appId: "1:1018154031940:web:937edbfb442a6dfc041511"
+};
+
+// Initialize Firebase (prevents double-initialization errors)
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
