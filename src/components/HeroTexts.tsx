3273493d854c590affcb31f64bad08781535ed38
sandeshjose01 <116247192+sandeshjose01@users.noreply.github.com>
2026-04-29 23:29:43 +0545

    Update HeroTexts.tsx

--- src/components/HeroTexts.tsx
+++ src/components/HeroTexts.tsx
@@ -12,15 +12,11 @@ const HeroTexts = ({ staticRole, roles, name }: HeroTextsProps) => {
   return (
     <>
       <h3 className="font-poppins text-2xl max-sm:text-xl text-white/70">
-        {/* // text: Fallback to "My Name is" if staticRole is empty */}
         {staticRole || "My Name is"}
       </h3>
       <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl text-white">
-        {/* // text: Dynamic Name from Admin Panel */}
         {name || "Sandesh Joshi"} .
       </h1>
-      
-      {/* // text: Passing the dynamic roles array to the limitless rotator */}
       <TextRotator items={roles || []} />
     </>
   );
