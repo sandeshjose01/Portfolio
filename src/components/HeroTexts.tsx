6625f21a3ef7b9bc9b4aa22d141dc80516663c38
sandeshjose01 <116247192+sandeshjose01@users.noreply.github.com>
2026-04-30 10:59:53 +0545

    Update HeroTexts.tsx

--- src/components/HeroTexts.tsx
+++ src/components/HeroTexts.tsx
@@ -11,13 +11,12 @@ interface HeroTextsProps {
 const HeroTexts = ({ roles, name, staticRole }: HeroTextsProps) => {
   return (
     <div className="flex flex-col items-start gap-2">
-      <h3 className="font-poppins text-2xl max-sm:text-xl text-white">
-        My Name is
+      <h3 className="font-poppins text-2xl max-sm:text-xl" >My Name is
       </h3>
       
       {/* // text: name_underline is your original CSS class. 
           // text: .split(" ").join("\n") forces the second name to a new line. */}
-      <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl text-white leading-[1.1] whitespace-pre-line">
+      <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl">
         {name ? name.split(" ").join("\n") : "Sandesh\nJoshi"} .
       </h1>
       
