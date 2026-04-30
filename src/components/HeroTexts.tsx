fb1ac5c80e2b1e0df2e8762af2b0da617efce284
sandeshjose01 <116247192+sandeshjose01@users.noreply.github.com>
2026-04-30 09:39:23 +0545

    Update HeroTexts.tsx

--- src/components/HeroTexts.tsx
+++ src/components/HeroTexts.tsx
@@ -1,20 +1,19 @@
+"use client";
+import React from "react";
 import TextRotator from "./TextRotator";
 
-// Your original H1
-<h1 className="..."> {name || "Sandesh \n Joshi ."} </h1>
- 
-const HeroTexts = () => {
- 
-  
+export default function HeroTexts({ name, staticRole, roles }: any) {
   return (
-    <>
-      <h3 className="font-poppins text-2xl max-sm:text-xl" >My Name is</h3>
-      <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl " >
-        {name || "Sandesh \n Joshi ."}
-      </h1>
-        <TextRotator/>
-     
-    </>
+    <div className="flex flex-col items-start gap-2">
+      <h3 className="font-poppins text-xl max-sm:text-xl" >My Name is</h3>
+      <div className="relative inline-block">
+        <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl">
+          {/* OR STATEMENT: Admin Name OR Original Hardcoded Name */}
+          {name ? name.replace(" ", "\n") : "Sandesh \n Joshi"} .
+        </h1>
+        <div className="absolute bottom-4 left-0 w-24 h-2 bg-[#2f7df4] rounded-full" />
+      </div>
+      <TextRotator staticRole={staticRole} items={roles} />
+    </div>
   );
-};
-export default HeroTexts
+}
